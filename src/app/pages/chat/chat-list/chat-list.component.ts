e9505f6f2c989c8a0d209ef327076d94bbf64eda
CarlesLopezMagem <carleslopezmagem@hotmail.com>
2020-04-14 08:50:07 +0200

    Init Chat

--- src/app/pages/chat/chat-list/chat-list.component.ts
+++ src/app/pages/chat/chat-list/chat-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { XatsService } from 'src/app/services/xats.service';
 import { Xat } from 'src/app/classes/xat';
+import { BaseApiService } from 'src/app/services/base-api.service';
+import { DjangoPaginationResult } from "src/app/classes/django-model";
 
 @Component({
   selector: 'app-chat-list',
@@ -9,16 +11,31 @@ import { Xat } from 'src/app/classes/xat';
 })
 export class ChatListComponent implements OnInit {
   public xats: Array<Xat> = [];
+  public next: string;
   constructor(
     public xatService: XatsService,
-  ) { }
+    protected api: BaseApiService,
+    ) { }
 
   ngOnInit() {
     this.loadXats();
   }
 
   protected loadXats() {
-    this.xatService.getAll().subscribe(xats => this.xats);
+    this.xatService.getAll().subscribe(result => this.xats = result.results);
+  }
+
+
+  public loadData($event) {
+    this.api.getUrl<DjangoPaginationResult<Xat>>(this.next).subscribe(result => {
+      this.updateResult(result);
+      $event.target.complete();
+    });
+  }
+
+  protected updateResult(result: DjangoPaginationResult<Xat>) {
+    this.xats = [...this.xats, ...result.results];
+    this.next = result.next;
   }
 
 }
--- src/app/pages/chat/chat-view/chat-view.component.ts
+++ src/app/pages/chat/chat-view/chat-view.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { XatsService } from 'src/app/services/xats.service';
+import { ActivatedRoute } from '@angular/router';
+import { Xat } from "src/app/classes/xat";
 
 @Component({
   selector: 'app-chat-view',
@@ -6,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat-view.component.scss'],
 })
 export class ChatViewComponent implements OnInit {
+  public xat: Xat;
 
-  constructor() { }
+  constructor(
+    public xatService: XatsService,
+    private route: ActivatedRoute,
+
+  ) { }
 
   ngOnInit() {}
 
+  ionViewDidEnter() {
+    this.loadTicket();
+  }
+  protected loadTicket() {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.xatService.get(id).subscribe(xat => this.xat = xat);
+  }
 }
